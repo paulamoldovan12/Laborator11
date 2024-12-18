f605f527aa16ea8a70c2e00963c3bb9ae2603f56
ZENBOOK\Paula <paula.a.moldovan12@gmail.com>
2024-12-18 19:58:22 +0200

    Add project files.

---
+++ Moldovan_Paula_Lab11/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Moldovan_Paula_Lab11.Data;
+
+namespace Moldovan_Paula_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Moldovan_Paula_Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Moldovan_Paula_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
